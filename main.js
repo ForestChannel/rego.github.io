7b4819dc886e70f0404802988d50748149275790
ForestChannel <45785169+ForestChannel@users.noreply.github.com>
2019-04-13 20:59:19 +0300

    Host

---
+++ main.js
@@ -0,0 +1,12 @@
+d = newDate
+h = d.getHours ()
+m = d.getMinutes ()
+s = d.getSeconds ()
+
+data = d.Date ()
+month = d.getMonth ()
+year = d.getFullYear ()
+
+document.write(h + m + s)
+function f1()
+document.getElementById("time").innerHTML="hello"
